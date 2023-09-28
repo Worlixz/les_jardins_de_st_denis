b91de0406a638c1e811804347b4411e9f9bef5e9
Worlixz <geremydumas@gmail.com>
2023-09-28 20:46:37 +0200

    pb pour build

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+// vite.config.js
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+})
